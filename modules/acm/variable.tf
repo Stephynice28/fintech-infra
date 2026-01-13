b370efaeb469e1d3d9272e23448513f3f2572f39
stephy27-nice <stephanimukum280@yahoo.com>
2026-01-13 09:22:50 -0500

    updated route53

--- modules/acm/variable.tf
+++ modules/acm/variable.tf
@@ -2,19 +2,19 @@
 variable "domain_name" {
   description = "Primary domain name for the certificate"
   type        = string
-  default     = "fusisoft.com"
+  default     = "beautanica.net"
 }
 
 variable "san_domains" {
   description = "Subject alternative names for the certificate"
   type        = list(string)
-  default     = ["*.fusisoft.com"]
+  default     = ["*.beautanica.net"]
 }
 
 variable "route53_zone_id" {
   description = "Route 53 Hosted Zone ID"
   type        = string
-  default     = "ZC7WDABJDII2" # Replace with actual Route 53 Zone ID
+  default     = "Z04679862B9I1N3EFO8UX" # Replace with actual Route 53 Zone ID
 }
 
 variable "tags" {
